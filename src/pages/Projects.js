61d5a03b3eb05fddb83164189299378fe779f377
John Attai <donattai1032@gmail>
2022-06-13 15:16:42 +0100

    deleted bootstrap container

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,27 +1,24 @@
 import ProjectItem from "../components/ProjectItem";
 import "../styles/Projects.css";
 import { ProjectList } from "../helpers/ProjectList";
-import { Container } from "react-bootstrap";
 
 function Projects() {
   return (
-    <Container>
-      <div className="projects">
-        <h1>Personal Projects</h1>
-        <div className="projectList ">
-          {ProjectList.map((project, index) => {
-            return (
-              <ProjectItem
-                key={index}
-                name={project.name}
-                image={project.image}
-                id={index}
-              />
-            );
-          })}
-        </div>
+    <div className="projects">
+      <h1>Personal Projects</h1>
+      <div className="projectList ">
+        {ProjectList.map((project, index) => {
+          return (
+            <ProjectItem
+              key={index}
+              name={project.name}
+              image={project.image}
+              id={index}
+            />
+          );
+        })}
       </div>
-    </Container>
+    </div>
   );
 }
 
